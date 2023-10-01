74d43250b5f50230295d87c19e018203980ac6a3
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2023-10-01 22:24:06 +0900

    add getter

--- human.hpp
+++ human.hpp
@@ -5,11 +5,18 @@
 #include <vector>
 
 class Human {
-  std::string name;
-  int age;
+  std::string _name;
+  int _age;
+  std::vector<std::shared_ptr<Human>> friends;
+  std::shared_ptr<Human> mother;
+  std::shared_ptr<Human> father;
 
 public:
-  Human() { std::cout << "おぎゃあ" << std::endl; }
+  Human() : _age(0) { born(); }
+  Human(std::string name) : _name(name), _age(0) { born(); }
+  void born() { std::cout << "おぎゃあ" << std::endl; }
+  std::string getName() { return _name; }
+  int getAge() { return _age; }
   ~Human() { std::cout << "さようなら" << std::endl; }
   virtual void live() = 0;
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "fujita.hpp"
 #include "human.hpp"
 #include <memory>
+#include <thread>
 #include <vector>
 
 int main() {
   std::vector<std::shared_ptr<Human>> people;
   std::shared_ptr<Human> fujita = std::make_shared<Fujita>();
   people.push_back(fujita);
-  people[0]->live();
+
+  for (auto &person : people) {
+    person->live();
+  }
 }
